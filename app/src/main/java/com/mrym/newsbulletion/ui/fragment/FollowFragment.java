56403096724d5895818c9360f3effdb170ec75a9
Jian ruilin <798774875@qq.com>
2016-11-20 22:43:38 +0800

    增加banner效果

--- app/src/main/java/com/mrym/newsbulletion/ui/fragment/FollowFragment.java
+++ app/src/main/java/com/mrym/newsbulletion/ui/fragment/FollowFragment.java
@@ -22,9 +22,14 @@ import com.mrym.newsbulletion.mvp.fragment.follow.FollowPresenter;
 import com.mrym.newsbulletion.mvp.fragment.follow.FollowView;
 import com.mrym.newsbulletion.ui.activity.PhotosDetailActivity;
 import com.mrym.newsbulletion.ui.activity.ViewPagerActivity;
+import com.mrym.newsbulletion.utils.GlideImageLoader;
 import com.mrym.newsbulletion.utils.common.ToastUtils;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.Bind;
@@ -44,6 +49,9 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
     private List<PhotoGirl> mPhotogirls;
     private PhotoGirlsAdapter mPhotoAdapter;
     private BaseRecyclerViewAdapter.onInternalClickListener onPhotoGirlsClick;
+    private ArrayList<String> images;
+    private String[] titles;
+
     @Override
     protected FollowPresenter createPresenter() {
         return new FollowPresenter(this);
@@ -59,10 +67,35 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mPhotogirls=new ArrayList<>();
-        mPhotoAdapter=new PhotoGirlsAdapter(mPhotogirls,getActivity());
+        mPhotogirls = new ArrayList<>();
+        mPhotoAdapter = new PhotoGirlsAdapter(mPhotogirls, getActivity());
         girlsList.setAdapter(mPhotoAdapter);
-        girlsList.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        images = new ArrayList<>();
+        images.add("http://f.hiphotos.baidu.com/news/q%3D100/sign=c0c87a1c7af082022b92953f7bfafb8a/4d086e061d950a7b6f8bd0fb03d162d9f2d3c957.jpg");
+        images.add("http://d.hiphotos.baidu.com/news/q%3D100/sign=ca91accda844ad3428bf8387e0a30c08/35a85edf8db1cb1312e63233d454564e92584b06.jpg");
+        titles = new String[]{"习近平出席亚太经合组织工商领导人峰会并发表主旨演讲", "习近平出席亚太经合组织领导人同工商咨询理事会代表对话会"};
+        View view = View.inflate(getActivity(), R.layout.fragment_banner, null);
+        Banner banner = (Banner) view.findViewById(R.id.banner);
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.NUM_INDICATOR_TITLE);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+        banner.setBannerTitles(Arrays.asList(titles));
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(3500);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+        girlsList.addHeaderView(view);
+        girlsList.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         girlsList.setEmptyView(View.inflate(getContext(), R.layout.item_empty_view, null));
         girlsList.setRefreshProgressStyle(ProgressStyle.BallBeat);
         girlsList.setLoadingMoreProgressStyle(ProgressStyle.BallBeat);
@@ -82,7 +115,7 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
         onPhotoGirlsClick = new BaseRecyclerViewAdapter.onInternalClickListener<PhotoGirl>() {
             @Override
             public void OnClickListener(View parentV, View v, Integer position, PhotoGirl values) {
-                PhotosDetailActivity.startAction(getActivity(),values.getUrl());
+                PhotosDetailActivity.startAction(getActivity(), values.getUrl());
             }
 
             @Override
@@ -92,6 +125,7 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
         };
         mPhotoAdapter.setOnInViewClickListener(R.id.item_grils_im, onPhotoGirlsClick);
     }
+
     public void switchActionAndLoadData(int action) {
         mCurrentAction = action;
         switch (mCurrentAction) {
@@ -106,6 +140,7 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
                 break;
         }
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -119,7 +154,7 @@ public class FollowFragment extends MvpFragment<FollowPresenter> implements Foll
 
     @Override
     public void loadingSuccess(List<PhotoGirl> girls) {
-        Log.i("FollowFragment",mPhotogirls.size()+"daxiao");
+        Log.i("FollowFragment", mPhotogirls.size() + "daxiao");
         mPhotoAdapter.addAll(girls);
     }
 
