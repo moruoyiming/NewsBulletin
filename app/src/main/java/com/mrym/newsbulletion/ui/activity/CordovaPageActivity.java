273cb6e26ad75a750c398a4378ad7b18279ca509
Jian ruilin <798774875@qq.com>
2016-12-01 22:22:07 +0800

    1.增加查看图片页面

---
+++ app/src/main/java/com/mrym/newsbulletion/ui/activity/CordovaPageActivity.java
@@ -0,0 +1,93 @@
+package com.mrym.newsbulletion.ui.activity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mrym.newsbulletion.NewsApplication;
+import com.mrym.newsbulletion.R;
+
+import org.apache.cordova.CordovaActivity;
+
+/**
+ * Created by Shawn on 2016/3/15.
+ */
+public class CordovaPageActivity extends CordovaActivity {
+
+    private static final String TAG = CordovaPageActivity.class.getCanonicalName();
+    public final static String INTENT_URL_KEY = "url_key";
+    private LinearLayout linearLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        NewsApplication.addActivity(this, TAG);
+        String url = getIntent().getStringExtra(INTENT_URL_KEY);
+        loadUrl(url);
+
+        // 如果加载外部网页，给页面添加Loading效果
+        if (url.startsWith("http")){
+            FrameLayout frameLayout = (FrameLayout) appView.getView().getParent();
+            linearLayout = new LinearLayout(this);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.gravity = Gravity.CENTER;
+            linearLayout.setLayoutParams(params);
+            linearLayout.setGravity(Gravity.CENTER);
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(linearLayout.getLayoutParams());
+            imageView.setImageResource(R.mipmap.loading);
+            // 加载动画
+            Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.loading_animation);
+            // 使用ImageView显示动画
+            imageView.startAnimation(hyperspaceJumpAnimation);
+            TextView textView = new TextView(this);
+            textView.setLayoutParams(linearLayout.getLayoutParams());
+            textView.setText("页面加载中，请稍候...");
+            textView.setTextColor(Color.parseColor("#DDDDDD"));
+            textView.setTextSize(14);
+            linearLayout.addView(imageView);
+            linearLayout.addView(textView);
+            frameLayout.addView(linearLayout);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        NewsApplication.removeActivity(TAG);
+        View webView = appView.getView();
+        // 解除WebView与父控件的依附关系
+        ((ViewGroup)webView.getParent()).removeView(webView);
+    }
+
+    @Override
+    public void onPageFinished(){
+        if (linearLayout!=null){
+            linearLayout.setVisibility(View.GONE);
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+//        AVAnalytics.onPause(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+//        AVAnalytics.onResume(this);
+    }
+}
