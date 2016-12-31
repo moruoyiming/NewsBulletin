a20a7bee2b699acc59bb3e2995d0d1a64a59523b
jianruilin <798774875@qq.com>
2016-12-31 20:27:23 +0800

    添加QQ登陆

--- app/src/main/java/com/mrym/newsbulletion/widget/LoadingTip.java
+++ app/src/main/java/com/mrym/newsbulletion/widget/LoadingTip.java
@@ -120,7 +120,7 @@ public class LoadingTip extends LinearLayout {
                 setVisibility(View.VISIBLE);
                 img_tip_logo.setVisibility(View.GONE);
                 progress.setVisibility(View.VISIBLE);
-                tv_tips.setText(getContext().getText(R.string.loading).toString());
+                tv_tips.setText(getContext().getText(R.string.please_wait).toString());
                 break;
             case finish:
                 setVisibility(View.GONE);
